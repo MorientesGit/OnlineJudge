ba5e7d5e198717021232e1019181028f08e422e2
MorientesGit <pablomorientesl@gmail.com>
2021-09-10 19:42:48 +0200

    DivisionOfNloginia

---
+++ CompetitiveProgramming3/SuperEasy/DivisionOfNLogonia.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main() {
+	int k, n, m, x, y;
+
+	while (cin >> k && k != 0) {
+		cin >> n >> m;
+		while (k--) {
+			cin >> x >> y;
+			if (x == n || y == m) cout << "divisa\n";
+			else if (x > n && y > m) cout << "NE\n";
+			else if (x > n && y < m) cout << "SE\n";
+			else if (x < n && y > m) cout << "NO\n";
+			else cout << "SO\n";
+		}
+	}
+}
